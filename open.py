7c8fce45c836c86bb9204d2148de7504408e225f
Tanmay Sawankar <137331988+TanmaySawankar390@users.noreply.github.com>
2025-02-09 12:43:27 +0530

    open.py

--- open.py
+++ open.py
@@ -93,4 +93,4 @@ def extract_qa():
     return jsonify({"error": "No valid image provided"}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(host='0.0.0.0',debug=True, port=8080)
